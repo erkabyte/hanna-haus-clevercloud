0eaaab1d4f2f6e14a27ad14bc520d7ada898ff83
erka b <erkabyte@gmail.com>
2019-11-02 23:24:41 +0800

    simple express server

---
+++ index.js
@@ -0,0 +1,18 @@
+
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get("/pubsub", (req,res) 
+	=>{
+
+	});
+
+app.post("/", (req,res) =>
+{
+
+})
+
+
+app.listen(port, () => "app is listening on 3000");
